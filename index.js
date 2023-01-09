6526d133fcaa00364f2b604d5b26159f0a0f4ec9
AntrikshNarang <itsantriksh@gmail.com>
2023-01-09 19:18:34 +0530

    Added Aside Section

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Search Focus and Focus Out
 document.querySelector(".sd-2").addEventListener("focusin",searchFocusIn);
 function searchFocusIn(){
     console.log("focusin");
@@ -9,4 +10,13 @@ function searchFocusOut(){
     console.log("focusout");
     document.querySelector(".sd-1").classList.remove("sd-1-onfocus");
     document.querySelector(".sd-2").classList.remove("sd-2-onfocus");
+}
+// ScrollBar implementation on aside hover
+document.querySelector(".aside-main").addEventListener("mouseover",showScrollBar);
+function showScrollBar(){
+    document.querySelector(".aside-main").classList.add("aside-scrbar");
+}
+document.querySelector(".aside-main").addEventListener("mouseout",hideScrollBar);
+function hideScrollBar(){
+    document.querySelector(".aside-main").classList.remove("aside-scrbar");
 }
